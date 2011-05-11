9c9273fcfd12a08b17291bafa3ccb7a0594404c4
Molepigeon <molepigeon@gmail.com>
2011-05-11 15:08:04 +0100

    Added file extensions (Should make computers happy)

--- source.asm
+++ source.asm
@@ -1,20 +1,23 @@
 ORG 40000
-CLEARSCREEN	LD	A,0
+CLEARSCREEN	; Code to clear the screen. Taken from Lab 3.
+		LD	A,0
 		;Colour bit. We want solid black for this.
 		LD	HL,4000h
 		;We're drawing to the display file, which
 		;starts at address 4000h.
 		LD	BC,1800h
-		;Draws 8px across?
+		;Draws on the entire screen.
 		LD	(HL),0
 		LD	D, H
 		LD	E, 1
 		LDIR ;Go to the start point.
 
-DRAWBLACKLINE	LD	A, 0
+DRAWBLACKLINE	;Draws a black line on the screen.
+		LD	A, 0
 		LD	(HL), A
 		;Dump colour bit to display file
 		LD	BC, 31
 		;Go across the first line
 		LDIR
+		;Loops to draw the line.
 		RET
