ad2868a4d4d16e1a6f727501ebebd3d4cebfbe7a
Chris Beesley <kireas@poketch.net>
2011-05-11 15:49:14 +0100

    Test code for question 3 - Non functional

--- source.asm
+++ source.asm
@@ -1,19 +1,18 @@
 ORG 40000
-CLEARSCREEN	; Code to clear the screen. Taken from Lab 3.
+INITALCLEAR	; Code to clear the screen. Taken from Lab 3.
 		LD	A,0
 		;Colour bit. We want solid black for this.
 		LD	HL,4000h
 		;We're drawing to the display file, which
 		;starts at address 4000h.
 		LD	BC,1800h
-		;Draws on the entire screen.
-		LD	(HL),0
+CLEARSCREEN	;Draws on the entire screen.
+		LD	(HL),
 		LD	D, H
 		LD	E, 1
 		LDIR ;Go to the start point.
 
-DRAWBLACKLINE	;Draws a black line on the screen.
-		LD	A, 0
+DRAWBLACKLINE	;Draws a black square on the screen.
 		LD	(HL), A
 		;Dump colour bit to display file
 		
@@ -22,8 +21,11 @@ DRAWBLACKLINE	;Draws a black line on the screen.
 		LD	A,7Fh
 		IN	A,(0FEh)
 		RRA
-		JP	NC,RUNAWAY
+		JP	NC,MOVEBLOCK
 
+		JP	INITALCLEAR
+
+MOVEBLOCK	INC	HL
 		JP	CLEARSCREEN
 
 RUNAWAY		RET
