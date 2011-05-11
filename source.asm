51ec339338e73030b91f8862b55c2d3ef39cbbfe
Chris Beesley <kireas@poketch.net>
2011-05-11 16:37:12 +0100

    Pressing Break will wipe the old block and create a new (but green) one. Not actually that helpful, but a good step

--- source.asm
+++ source.asm
@@ -6,8 +6,8 @@ INITALCLEAR	; Code to clear the screen. Taken from Lab 3.
 		;We're drawing to the display file, which
 		;starts at address 4000h.
 		LD	BC,1800h
-CLEARSCREEN	;Draws on the entire screen.
-		LD	(HL),
+		;Draws on the entire screen.
+		LD	(HL),0
 		LD	D, H
 		LD	E, 1
 		LDIR ;Go to the start point.
@@ -21,11 +21,11 @@ DRAWBLACKLINE	;Draws a black square on the screen.
 		LD	A,7Fh
 		IN	A,(0FEh)
 		RRA
-		JP	NC,MOVEBLOCK
+		JP	NC,CLEARBLOCK
 
 		JP	INITALCLEAR
 
-MOVEBLOCK	INC	HL
-		JP	CLEARSCREEN
-
-RUNAWAY		RET
+CLEARBLOCK	LD	(HL),56	
+		INC	L	
+		LD	(HL),32
+		RET
