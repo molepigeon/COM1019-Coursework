cde05e8ca5936d7dc4f290b05a72a061a12e9d3e
Chris Beesley <kireas@poketch.net>
2011-05-12 16:50:32 +0100

    Can now move left and right (AT HIGH SPEED) using A and D.

--- source.asm
+++ source.asm
@@ -15,17 +15,28 @@ INITALCLEAR	; Code to clear the screen. Taken from Lab 3.
 DRAWBLACKLINE	;Draws a black square on the screen.
 		LD	(HL), A
 		;Dump colour bit to display file
-		
-		OR	C
+		JP	HOLDINGPATTERN
 
-		LD	A,7Fh
+STEPRIGHT	LD	(HL),56
+		;Set HL to a blank colour
+		INC	L
+		;Move to the next block	
+		LD	(HL),0
+		;Make the new block black
+		LD	BC,255
+		;Load B with a wait for the next jump
+		JP	WAIT
+
+WAIT		DEC	BC
+		LD	A,0
+		ADD	B,A
+		JP	NZ,WAIT
+		JP	HOLDINGPATTERN
+
+HOLDINGPATTERN	OR	C
+
+		LD	A,7h
 		IN	A,(0FEh)
 		RRA
-		JP	NC,CLEARBLOCK
-
-		JP	INITALCLEAR
-
-CLEARBLOCK	LD	(HL),56	
-		INC	L	
-		LD	(HL),32
-		RET
+		JP	NC,STEPRIGHT
+		JP	HOLDINGPATTERN
